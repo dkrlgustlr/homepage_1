24bd3ec3c1d2fc5ab5498c1a59d68c04ee210bb5
dkrlgustlr <dkrlgustlr@users.noreply.github.com>
2026-06-24 21:58:52 +0900

    Equalize mobile case tab heights

--- layout.js
+++ layout.js
@@ -1,5 +1,5 @@
 (() => {
-  const INCLUDE_VERSION = "20260624-mobile17";
+  const INCLUDE_VERSION = "20260624-mobile18";
   const includes = [
     ["[data-include='header']", `header.html?v=${INCLUDE_VERSION}`],
     ["[data-include='footer']", `footer.html?v=${INCLUDE_VERSION}`]
