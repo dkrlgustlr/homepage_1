4cffd6c8d4b8ee21b3a23b01e531b7e1dfc5d5da
dkrlgustlr <dkrlgustlr@users.noreply.github.com>
2026-06-25 02:04:30 +0900

    fix: balance mobile case tab sizing

--- layout.js
+++ layout.js
@@ -1,5 +1,5 @@
 (() => {
-  const INCLUDE_VERSION = "20260625-mobile34";
+  const INCLUDE_VERSION = "20260625-mobile35";
   const includes = [
     ["[data-include='header']", `header.html?v=${INCLUDE_VERSION}`],
     ["[data-include='footer']", `footer.html?v=${INCLUDE_VERSION}`]
