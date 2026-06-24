db4b84ac3ad1d31b99e05e25224e854564d5c96b
dkrlgustlr <dkrlgustlr@users.noreply.github.com>
2026-06-25 02:44:44 +0900

    fix: pin mobile nav panel to top right

--- layout.js
+++ layout.js
@@ -1,5 +1,5 @@
 (() => {
-  const INCLUDE_VERSION = "20260625-mobile36";
+  const INCLUDE_VERSION = "20260625-mobile37";
   const includes = [
     ["[data-include='header']", `header.html?v=${INCLUDE_VERSION}`],
     ["[data-include='footer']", `footer.html?v=${INCLUDE_VERSION}`]
