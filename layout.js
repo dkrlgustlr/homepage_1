56ecfdbb716ebb3405c40c8f58e52f0ae065c4be
dkrlgustlr <dkrlgustlr@users.noreply.github.com>
2026-06-24 22:09:41 +0900

    Add 768px mobile layout layer

--- layout.js
+++ layout.js
@@ -1,5 +1,5 @@
 (() => {
-  const INCLUDE_VERSION = "20260624-mobile18";
+  const INCLUDE_VERSION = "20260624-mobile19";
   const includes = [
     ["[data-include='header']", `header.html?v=${INCLUDE_VERSION}`],
     ["[data-include='footer']", `footer.html?v=${INCLUDE_VERSION}`]
